c2876e7fc4ec16e81075b5b9e4d2798f146b3e93
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-09 16:53:42 -0500

    updated

--- array-filtering.js
+++ array-filtering.js
@@ -1,5 +1,9 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const filtered = numbers.filter(function evenNumbers (number) {
+const evenNumbers = function(number) {
 	return number % 2 === 0
-});
+}
+const oddNumbers = function (number) {
+	return number % 2 !== 0
+}
+const filtered = numbers.filter(evenNumbers);
 console.log(filtered);
